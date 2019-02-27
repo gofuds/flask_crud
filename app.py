0687f432d00479076eeb44a22f61d320015787c2
hyeryung suh <gofuds@naver.com>
2019-02-27 06:12:27 +0000

    01_flask&flask-sqlalchemy init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    content = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
